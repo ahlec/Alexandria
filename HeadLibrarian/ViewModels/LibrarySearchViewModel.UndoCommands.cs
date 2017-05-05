a97d0e1547913d0b242f59c80a421418dd1a4f19
Alec Deitloff <alec.deitloff@gmail.com>
2017-05-05 07:57:57 -0700

    + Added a number of properties to LibrarySearchView and LibrarySearchViewModel + Broke a single row of the LibrarySearchView out into a reusable control to allow for easier adding of rows and also customisation issues

--- HeadLibrarian/ViewModels/LibrarySearchViewModel.UndoCommands.cs
+++ HeadLibrarian/ViewModels/LibrarySearchViewModel.UndoCommands.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Alexandria.Model;
 
 namespace HeadLibrarian.ViewModels
 {
@@ -28,6 +29,136 @@ namespace HeadLibrarian.ViewModels
 			readonly LibrarySearchViewModel _viewModel;
 			readonly String _oldName;
 			readonly String _newName;
+		}
+
+		class SetAuthorUndoAction : IUndoRedoAction
+		{
+			public SetAuthorUndoAction( LibrarySearchViewModel viewModel, String oldAuthor, String newAuthor )
+			{
+				_viewModel = viewModel;
+				_oldAuthor = oldAuthor;
+				_newAuthor = newAuthor;
+			}
+
+			public void Undo()
+			{
+				_viewModel._search.Author = _oldAuthor;
+				_viewModel.InvokeAuthorChanged();
+			}
+
+			public void Redo()
+			{
+				_viewModel._search.Author = _newAuthor;
+				_viewModel.InvokeAuthorChanged();
+			}
+
+			readonly LibrarySearchViewModel _viewModel;
+			readonly String _oldAuthor;
+			readonly String _newAuthor;
+		}
+
+		class SetOnlyIncludeCompleteUndoAction : IUndoRedoAction
+		{
+			public SetOnlyIncludeCompleteUndoAction( LibrarySearchViewModel viewModel, Boolean oldValue, Boolean newValue )
+			{
+				_viewModel = viewModel;
+				_oldValue = oldValue;
+				_newValue = newValue;
+			}
+
+			public void Undo()
+			{
+				_viewModel._search.OnlyIncludeCompleteFanfics = _oldValue;
+				_viewModel.InvokeOnlyIncludeCompleteChanged();
+			}
+
+			public void Redo()
+			{
+				_viewModel._search.OnlyIncludeCompleteFanfics = _newValue;
+				_viewModel.InvokeOnlyIncludeCompleteChanged();
+			}
+
+			readonly LibrarySearchViewModel _viewModel;
+			readonly Boolean _oldValue;
+			readonly Boolean _newValue;
+		}
+
+		class SetOnlyIncludeSingleChapterUndoAction : IUndoRedoAction
+		{
+			public SetOnlyIncludeSingleChapterUndoAction( LibrarySearchViewModel viewModel, Boolean oldValue, Boolean newValue )
+			{
+				_viewModel = viewModel;
+				_oldValue = oldValue;
+				_newValue = newValue;
+			}
+
+			public void Undo()
+			{
+				_viewModel._search.OnlyIncludeSingleChapterFanfics = _oldValue;
+				_viewModel.InvokeOnlyIncludeSingleChapterChanged();
+			}
+
+			public void Redo()
+			{
+				_viewModel._search.OnlyIncludeSingleChapterFanfics = _newValue;
+				_viewModel.InvokeOnlyIncludeSingleChapterChanged();
+			}
+
+			readonly LibrarySearchViewModel _viewModel;
+			readonly Boolean _oldValue;
+			readonly Boolean _newValue;
+		}
+
+		class SetLanguageUndoAction : IUndoRedoAction
+		{
+			public SetLanguageUndoAction( LibrarySearchViewModel viewModel, Language? oldLanguage, Language? newLanguage )
+			{
+				_viewModel = viewModel;
+				_oldLanguage = oldLanguage;
+				_newLanguage = newLanguage;
+			}
+
+			public void Undo()
+			{
+				_viewModel._search.Language = _oldLanguage;
+				_viewModel.InvokeLanguageChanged();
+			}
+
+			public void Redo()
+			{
+				_viewModel._search.Language = _newLanguage;
+				_viewModel.InvokeLanguageChanged();
+			}
+
+			readonly LibrarySearchViewModel _viewModel;
+			readonly Language? _oldLanguage;
+			readonly Language? _newLanguage;
+		}
+
+		class SetMaturityRatingUndoAction : IUndoRedoAction
+		{
+			public SetMaturityRatingUndoAction( LibrarySearchViewModel viewModel, MaturityRating? oldMaturityRating, MaturityRating? newMaturityRating )
+			{
+				_viewModel = viewModel;
+				_oldMaturityRating = oldMaturityRating;
+				_newMaturityRating = newMaturityRating;
+			}
+
+			public void Undo()
+			{
+				_viewModel._search.Rating = _oldMaturityRating;
+				_viewModel.InvokeMaturityRatingChanged();
+			}
+
+			public void Redo()
+			{
+				_viewModel._search.Rating = _newMaturityRating;
+				_viewModel.InvokeMaturityRatingChanged();
+			}
+
+			readonly LibrarySearchViewModel _viewModel;
+			readonly MaturityRating? _oldMaturityRating;
+			readonly MaturityRating? _newMaturityRating;
 		}
 	}
 }
--- HeadLibrarian/ViewModels/LibrarySearchViewModel.cs
+++ HeadLibrarian/ViewModels/LibrarySearchViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Alexandria.Model;
 using Alexandria.Searching;
 
 namespace HeadLibrarian.ViewModels
@@ -32,6 +34,134 @@ namespace HeadLibrarian.ViewModels
 		{
 			OnPropertyChanged( nameof( Title ) );
 			_projectViewModel.RefreshHasSavedChanged();
+		}
+
+		public String Author
+		{
+			get => _search.Author;
+			set
+			{
+				String oldAuthor = Author;
+				if ( String.Equals( oldAuthor, value, StringComparison.CurrentCultureIgnoreCase ) )
+				{
+					return;
+				}
+
+				_search.Author = value;
+				_projectViewModel.UndoStack.Push( new SetAuthorUndoAction( this, oldAuthor, value ) );
+				InvokeAuthorChanged();
+			}
+		}
+
+		void InvokeAuthorChanged()
+		{
+			OnPropertyChanged( nameof( Author ) );
+			_projectViewModel.RefreshHasSavedChanged();
+		}
+
+		public Boolean OnlyIncludeComplete
+		{
+			get => _search.OnlyIncludeCompleteFanfics;
+			set
+			{
+				Boolean oldValue = OnlyIncludeComplete;
+				if ( value == oldValue )
+				{
+					return;
+				}
+
+				_search.OnlyIncludeCompleteFanfics = value;
+				_projectViewModel.UndoStack.Push( new SetOnlyIncludeCompleteUndoAction( this, oldValue, value ) );
+				InvokeOnlyIncludeCompleteChanged();
+			}
+		}
+
+		void InvokeOnlyIncludeCompleteChanged()
+		{
+			OnPropertyChanged( nameof( OnlyIncludeComplete ) );
+			_projectViewModel.RefreshHasSavedChanged();
+		}
+
+		public Boolean OnlyIncludeSingleChapter
+		{
+			get => _search.OnlyIncludeSingleChapterFanfics;
+			set
+			{
+				Boolean oldValue = OnlyIncludeSingleChapter;
+				if ( value == oldValue )
+				{
+					return;
+				}
+
+				_search.OnlyIncludeSingleChapterFanfics = value;
+				_projectViewModel.UndoStack.Push( new SetOnlyIncludeSingleChapterUndoAction( this, oldValue, value ) );
+				InvokeOnlyIncludeSingleChapterChanged();
+			}
+		}
+
+		void InvokeOnlyIncludeSingleChapterChanged()
+		{
+			OnPropertyChanged( nameof( OnlyIncludeSingleChapter ) );
+			_projectViewModel.RefreshHasSavedChanged();
+		}
+
+		public IEnumerable<Language?> AllLanguages { get; } = GetAllNullableEnumValues<Language>();
+
+		public Language? Language
+		{
+			get => _search.Language;
+			set
+			{
+				Language? oldLanguage = Language;
+				if ( oldLanguage == value )
+				{
+					return;
+				}
+
+				_search.Language = value;
+				_projectViewModel.UndoStack.Push( new SetLanguageUndoAction( this, oldLanguage, value ) );
+				InvokeLanguageChanged();
+			}
+		}
+
+		void InvokeLanguageChanged()
+		{
+			OnPropertyChanged( nameof( Language ) );
+			_projectViewModel.RefreshHasSavedChanged();
+		}
+
+		public IEnumerable<MaturityRating?> AllMaturityRatings { get; } = GetAllNullableEnumValues<MaturityRating>();
+
+		public MaturityRating? MaturityRating
+		{
+			get => _search.Rating;
+			set
+			{
+				MaturityRating? oldMaturityRating = MaturityRating;
+				if ( oldMaturityRating == value )
+				{
+					return;
+				}
+
+				_search.Rating = value;
+				_projectViewModel.UndoStack.Push( new SetMaturityRatingUndoAction( this, oldMaturityRating, value ) );
+				InvokeMaturityRatingChanged();
+			}
+		}
+
+		void InvokeMaturityRatingChanged()
+		{
+			OnPropertyChanged( nameof( MaturityRating ) );
+			_projectViewModel.RefreshHasSavedChanged();
+		}
+
+		static IEnumerable<T?> GetAllNullableEnumValues<T>() where T : struct, IConvertible
+		{
+			yield return null;
+			foreach ( T enumValue in Enum.GetValues( typeof( T ) ) )
+			{
+				yield return enumValue;
+			}
 		}
 
 		readonly ProjectViewModel _projectViewModel;
