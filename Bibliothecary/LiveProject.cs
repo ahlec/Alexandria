92e335d1e322236c5b6cec9840663a2443904681
Alec Deitloff <alec.deitloff@gmail.com>
2017-05-22 21:52:26 -0700

    + Began working on Bibliothecary now that we have a base program to modify our service data with. + Began working on standing up the barebone basics of Bibliothecary. More work to come, and I can't even verify that this works correctly. However, it compiles, and it begins to show the basics of what we're looking to do with it.

---
+++ Bibliothecary/LiveProject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Bibliothecary.Data;
+
+namespace Bibliothecary
+{
+	internal sealed class LiveProject
+	{
+		public LiveProject( Project project )
+		{
+			Project = project;
+			ResetCountdown();
+		}
+
+		public Project Project { get; }
+
+		public TimeSpan TimeRemaining { get; private set; }
+
+		public Boolean IsTimeToSearch { get; private set; }
+
+		public void ResetCountdown()
+		{
+			IsTimeToSearch = false;
+			TimeRemaining = Project.UpdateFrequency;
+		}
+
+		public void Update( TimeSpan elapsedTime )
+		{
+			if ( IsTimeToSearch )
+			{
+				return;
+			}
+
+			if ( elapsedTime > TimeRemaining )
+			{
+				IsTimeToSearch = true;
+				return;
+			}
+
+			TimeRemaining -= elapsedTime;
+		}
+	}
+}
