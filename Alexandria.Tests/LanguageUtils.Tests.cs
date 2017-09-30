af91cc613afd3438b86c82425b2514ca79d98c7b
Alec Deitloff <alec.deitloff@gmail.com>
2017-09-29 20:30:21 -0700

    + Updating project headers to point to new GitHub location instead of old Bitbucket repository

--- Alexandria.Tests/LanguageUtils.Tests.cs
+++ Alexandria.Tests/LanguageUtils.Tests.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// This code is part of the Alexandria project (https://bitbucket.org/ahlec/alexandria/).
+// This code is part of the Alexandria project (https://github.com/ahlec/Alexandria).
 // Written and maintained by Alec Deitloff.
 // Archive of Our Own (https://archiveofourown.org) is owned by the Organization for Transformative Works (http://www.transformativeworks.org/).
 // -----------------------------------------------------------------------
